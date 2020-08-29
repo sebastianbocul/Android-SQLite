581aae1a5c92eec4755647bf08a7368dc03ca037
sebastianbocul <seba23495@gmail.com>
2020-08-29 02:28:37 +0200

    Created Room NoteDatabase

---
+++ app/src/main/java/com/sebix/android_sqlite/persistance/NoteDatabase.java
@@ -0,0 +1,26 @@
+package com.sebix.android_sqlite.persistance;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.sebix.android_sqlite.models.Note;
+
+@Database(entities = {Note.class}, version = 1)
+public abstract class NoteDatabase extends RoomDatabase {
+    public static final String DATABASE_NAME = "notes_ds";
+    public static NoteDatabase instance;
+
+    static NoteDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(
+                    context.getApplicationContext(),
+                    NoteDatabase.class,
+                    DATABASE_NAME
+            ).build();
+        }
+        return instance;
+    }
+}
