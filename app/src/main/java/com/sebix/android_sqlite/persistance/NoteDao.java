a771da5c2550926f7df5a638cbba3928106aeebf
sebastianbocul <seba23495@gmail.com>
2020-08-29 02:05:09 +0200

    Created notesDao

---
+++ app/src/main/java/com/sebix/android_sqlite/persistance/NoteDao.java
@@ -0,0 +1,28 @@
+package com.sebix.android_sqlite.persistance;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.sebix.android_sqlite.models.Note;
+
+import java.util.List;
+
+@Dao
+interface NoteDao {
+    @Insert
+    long[] insertNotes(Note...notes);
+
+    @Query("SELECT * FROM notes")
+    LiveData<List<Note>> getNotes();
+
+
+    @Delete
+    int deleteNotes(Note...notes);
+
+    @Update
+    int updateNotes(Note ... notes);
+}
